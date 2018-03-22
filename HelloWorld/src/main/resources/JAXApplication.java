206cf71254e366d4e4718a8cfde7655d0416e7bb
Piotr Maciaszek <Piotr Maciaszek@PCPMACIASZEK.msp.comarch>
2018-03-22 10:45:33 +0100

    test

---
+++ HelloWorld/src/main/resources/JAXApplication.java
@@ -0,0 +1,14 @@
+/* @(#) $Id$
+ *
+ * Copyright (c) 2000-2018 ComArch S.A. All Rights Reserved. Any usage, duplication or redistribution of this
+ * software is allowed only according to separate agreement prepared in written between ComArch and authorized party.
+ */
+
+/**
+ * @author Piotr Maciaszek
+ */
+
+@ApplicationPath("/rest")
+public class JAXApplication extends Application {
+    
+}
