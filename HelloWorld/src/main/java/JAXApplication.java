e14a531018a52df1616776545abc1cef6733d839
Piotr Maciaszek <Piotr Maciaszek@PCPMACIASZEK.msp.comarch>
2018-03-22 11:07:06 +0100

    test

--- HelloWorld/src/main/java/JAXApplication.java
+++ HelloWorld/src/main/java/JAXApplication.java
@@ -1,3 +1,6 @@
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
 /* @(#) $Id$
  *
  * Copyright (c) 2000-2018 ComArch S.A. All Rights Reserved. Any usage, duplication or redistribution of this
